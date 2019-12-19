1b86fd312fb21193679ef7ca0899029f2b6da27b
Prashanth Nandavanam <pn@forwardmeasure.com>
2019-12-18 19:02:33 -0500

    Update conanfile.py

--- recipes/conan-abseil/conanfile.py
+++ recipes/conan-abseil/conanfile.py
@@ -23,8 +23,7 @@ class AbseilConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     requires = "cctz/2.3@bincrafters/stable"
     options = {"cxx_standard": [11, 14, 17], "build_testing": [True, False], "fPIC" : [True, False]}
-    default_options = {"cxx_standard": 11, "build_testing": False, "fPIC": True}
-    short_paths = True
+    default_options = {"cxx_standard": 17, "build_testing": False, "fPIC": True}
     _source_subfolder = "source_subfolder"
 
     def source(self):
@@ -32,15 +31,11 @@ class AbseilConan(ConanFile):
         extracted_dir = "abseil-cpp-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
     def configure(self):
         if self.settings.os == "Windows" and \
            self.settings.compiler == "Visual Studio" and \
            Version(self.settings.compiler.version.value) < "14":
-            raise ConanInvalidConfiguration("Abseil does not support MSVC < 14")
+               raise ConanInvalidConfiguration("Abseil does not support MSVC < 14")
 
     def build(self):
         cmake = CMake(self)
@@ -50,6 +45,13 @@ class AbseilConan(ConanFile):
         cmake.definitions["ABSL_CCTZ_TARGET"] = "CONAN_PKG::cctz"
         cmake.configure()
         cmake.build()
+
+    def _rename_libs(self):
+        lib_dir = os.path.abspath("lib")
+        for lib_name in os.listdir(lib_dir):
+            new_lib_name = lib_name.replace("libabsl_absl", "libabsl")
+            print ("Renaming %s to %s" % (lib_name, new_lib_name))
+            os.rename(os.path.abspath(os.path.join(lib_dir, lib_name)), os.path.abspath(os.path.join(lib_dir, new_lib_name)))
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
@@ -63,39 +65,9 @@ class AbseilConan(ConanFile):
     def package_info(self):
         if self.settings.os == "Linux":
             self.cpp_info.libs = ["-Wl,--start-group"]
-        self.cpp_info.libs.extend(["absl_dynamic_annotations",
-                                   "absl_internal_spinlock_wait",
-                                   "absl_bad_optional_access",
-                                   "absl_leak_check",
-                                   "absl_leak_check_disable",
-                                   "absl_container",
-                                   "absl_internal_city",
-                                   "pow10_helper",
-                                   "absl_bad_any_cast",
-                                   "absl_int128",
-                                   "absl_numeric",
-                                   "absl_base",
-                                   "absl_internal_throw_delegate",
-                                   "absl_utility",
-                                   "absl_meta",
-                                   "absl_internal_malloc_internal",
-                                   "absl_optional",
-                                   "absl_span",
-                                   "absl_demangle_internal",
-                                   "absl_internal_debugging_internal",
-                                   "absl_variant",
-                                   "absl_any",
-                                   "absl_strings",
-                                   "absl_stacktrace",
-                                   "absl_symbolize",
-                                   "absl_hash",
-                                   "str_format_extension_internal",
-                                   "absl_raw_hash_set",
-                                   "absl_internal_examine_stack",
-                                   "str_format_internal",
-                                   "absl_failure_signal_handler",
-                                   "absl_str_format",
-                                   "absl_time",
-                                   "absl_synchronization"])
+
+        self.cpp_info.libs.extend(tools.collect_libs(self))
+
         if self.settings.os == "Linux":
-            self.cpp_info.libs.append("pthread")
+            self.cpp_info.libs.extend(["-Wl,--end-group", "pthread"])
+        self._rename_libs()

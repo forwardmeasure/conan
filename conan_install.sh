cb3f6d1d6c760ece05f82c5dfc535f51a04bb44a
Prashanth Nandavanam <pn@forwardmeasure.com>
2020-02-21 06:40:52 -0500

    Added script fo rgit invocation to avoid load library path issues.

--- conan_install.sh
+++ conan_install.sh
@@ -7,9 +7,7 @@ export SCRIPTS_DIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}/" )"; pwd )"
 CONAN_CHANNEL=@bottomline/stable
 BUILD_TYPE=Release
 CONANFILE_TXT=conanfile.txt
-CONANFILE_TF_TXT=conanfile-tf.txt
-LIBS_TO_EXPORT="openssl asio bzip2 cctz abseil boost c-ares gtest poco xtl xtensor xframe onnx protobuf grpc websocketpp cpprestsdk expat apache-apr apache-apr-util apache-log4cxx json4moderncpp xgboost outcome tensorflow"
-while getopts "d:l:i:o:e:c:f:b:l:" opt; do
+while getopts "o:e:c:f:b:l:" opt; do
   case ${opt} in
      o)
       OPTIM_SPEC=$OPTARG
@@ -28,12 +26,6 @@ while getopts "d:l:i:o:e:c:f:b:l:" opt; do
       ;;
      f)
       CONANFILE_TXT=$OPTARG
-      ;;
-     d)
-      CONAN_FILE_BASE_DIR=$OPTARG
-      ;;
-     i)
-      LIBS_TO_EXPORT=$OPTARG
       ;;
 	 \?)
       echo "Invalid option: $OPTARG" 1>&2
@@ -77,19 +69,10 @@ then
 	COMPILER=gcc_8
 fi
 
-if [ "x$CONAN_FILE_BASE_DIR" = "x" ]
-then
-    echo "Error, you must provide the location of the base directory under which the Conan recipe folders reside."
-    exit 1
-fi
-
 EXTRA_BUILD_SPEC=$(get_extra_build_spec $COMPILER $COMPUTE_ENGINE $OPTIM_SPEC)
 BUILD_PROFILE=$UNAME-$EXTRA_BUILD_SPEC
 PROFILE_FILE="${SCRIPTS_DIR}/profiles/$BUILD_PROFILE"
 INSTALL_FOLDER=${CONAN_USER_HOME}/CMakeModules/${EXTRA_BUILD_SPEC}
 
-# Export the requires recipes
-do_conan_export $CONAN_CHANNEL $CONAN_FILE_BASE_DIR "$LIBS_TO_EXPORT"
-
-# Install all packages except TF
+conan profile update settings.compiler.libcxx=libstdc++11 default
 do_conan_install $CONANFILE_TXT $CONAN_CHANNEL $BUILD_TYPE $COMPILER $COMPUTE_ENGINE $OPTIM_SPEC $PROFILE_FILE $INSTALL_FOLDER
---
+++ functions.sh
@@ -0,0 +1,296 @@
+UNAME=$(tr [A-Z] [a-z] <<< "$(uname)")
+export SCRIPTS_DIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}/" )"; pwd )"
+
+function get_default_compiler()
+{
+	# Detect the platform (similar to $OSTYPE)
+	OS="`uname`"
+	case $OS in
+  	'Linux')
+		echo "gcc"
+    	;;
+  	'FreeBSD')
+		echo "gcc"
+    	;;
+  	'WindowsNT')
+		echo "msvcpp"
+    	;;
+  	'Darwin') 
+		echo "clang"
+    	;;
+  	'SunOS')
+    	echo "gcc"
+    	;;
+  	'AIX') 
+		echo "xlc"
+    	;;
+  	*)
+		echo "UNKNOWN"
+		;;
+	esac
+}
+
+############################################################################
+# Get the build spec 
+#
+# Syntax: get_extra_build_spec <compiler> <compute_engine> <optimization spec>
+# e.g. get_extra_build_spec "gcc" "cpu" "all"
+############################################################################
+function get_extra_build_spec()
+{
+	local compiler=$1
+	local compute_engine=$2
+	local optim_spec=$3
+
+	local build_spec="${compiler}-${compute_engine}-${optim_spec}"
+	echo $build_spec
+}
+
+############################################################################
+# Remove duplicates and things that don't exist
+#
+# Syntax: cleanupPath PATHNAME
+############################################################################
+cleanupPath()
+{
+    pathName=$1
+    eval pathContents=\"\$$pathName\"
+	newPath=$(echo -n $pathContents | awk -v RS=: '!($0 in a) {a[$0]; printf("%s%s", length(a) > 1 ? ":" : "", $0)}')
+    eval "$pathName=$newPath"
+}
+
+############################################################################
+# Exports Conan recipes from a given directory
+#
+# Syntax: conan_export <Conan channel> <Base Directory> <List of packages>
+############################################################################
+function do_conan_export()
+{
+	conan_channel=$1
+	base_dir=$2
+	libs_to_build=$3
+	
+	myName=$(basename $0)
+	for i in ${libs_to_build}
+	do
+		dir="${base_dir}/${i}"
+		if [[ ! -d "${dir}" ]]
+		then
+			dir="${base_dir}/conan-${i}"
+		fi
+
+		if [[ -d ${dir} ]]
+		then
+			dirBase="$(basename -- ${dir})"
+			echo "Exporting recipe for package $dirBase"
+
+			conan export ${dir} ${conan_channel}
+		fi
+	done
+}
+
+
+git()
+{   
+	unset git &> /dev/null 
+	unalias git &> /dev/null
+	unset -f git &> /dev/null
+
+
+    gitRealPath=$(command -v git.exe)
+    declare -A git_libs
+
+	for i in `ldd ${gitRealPath} | grep so | sed -e '/^[^\t]/ d' | sed -e 's/\t//' | sed -e 's/.*=..//' | sed -e 's/ (0.*)//' | sort | uniq`
+    do
+        lib_dir=$(dirname $i)
+        echo $lib_dir
+        if [[ $lib_dir == /* ]]
+        then
+            git_libs["$lib_dir"]="$lib_dir"
+        fi
+    done
+
+    lib_path=""
+    for x in "${!git_libs[@]}"
+    do  
+        printf "[%s]=%s\n" "$x" "${git_libs[$x]}"
+        
+        if [[ ${lib_path} == "" ]]
+        then
+            lib_path=$x
+        else
+            lib_path="$lib_path:$x"
+        fi
+    done
+
+    cmd="LD_LIBRARY_PATH=${lib_path};DYLD_LIBRARY_PATH=${lib_path} ${gitRealPath} $@"
+	echo "Running git with adjusted LIBRARY paths variables as $cmd"
+	eval ${cmd}
+}
+
+git()
+{
+	gitRealPath=$(command -v git)
+	declare -A git_libs
+	for i in `ldd ${gitRealPath} | grep so | sed -e '/^[^\t]/ d' | sed -e 's/\t//' | sed -e 's/.*=..//' | sed -e 's/ (0.*)//' | sort | uniq`
+	do
+		lib_dir=$(dirname $i)
+		echo $lib_dir
+		if [[ $lib_dir == /* ]]
+		then
+			git_libs["$lib_dir"]="$lib_dir"
+		fi
+	done
+
+	lib_path=""
+	for x in "${!git_libs[@]}"
+	do
+		printf "[%s]=%s\n" "$x" "${git_libs[$x]}"
+
+		if [[ ${lib_path} == "" ]]
+		then
+			lib_path=$x
+		else
+			lib_path="$lib_path:$x"
+		fi
+	done	
+
+#   echo "Running GIT command": "LD_LIBRARY_PATH=${lib_path};DYLD_LIBRARY_PATH=${lib_path}" $gitRealPath "$@"
+	LD_LIBRARY_PATH="${lib_path};DYLD_LIBRARY_PATH=${lib_path}" command git "$@"
+}
+
+######################################################################################
+# Installs a given set of Conan recipes
+#
+# Syntax: do_conan_install <conanfile> <Conan channel> <build type> <compiler> <compute engine> \
+#                          <optimization spec> <Conan profile> <Conan install folder>
+######################################################################################
+function do_conan_install()
+{
+	conanfile_txt=$1
+	conan_channel=$2
+	build_type=$3
+	compiler=$4
+	compute_engine=$5
+	optim_spec=$6
+	profile_file=$7
+	install_folder=$7
+	
+	if ! [[ "$optim_spec" =~ ^(all|safe|none)$ ]]
+	then
+		echo "Error: unknown optimisation level '$optim_spec'"
+		return 1
+	fi
+	
+	if ! [[ "$compute_engine" =~ ^(cpu|cuda_10)$ ]]
+	then
+		echo "Error: unknown compute engine type '$compute_engine'"
+		return 1
+	fi
+	
+	if [ "x$compiler" = "x" ]
+	then
+		echo "Error: no compiler specified"
+		return 1
+	fi
+
+	echo "Using profile '$profile_file' for building"
+	if [ ! -f "$profile_file" ]
+	then
+		echo "Error: profile file $profile_file does not exist"
+		return 1
+	fi
+	
+	if [ ! -f "$conanfile_txt" ]
+	then
+		echo "Error: conanfile $conanfile_txt does not exist"
+		return 1
+	fi
+
+	extra_build_spec=$(get_extra_build_spec $compiler $compute_engine $optim_spec)
+	install_folder=${CONAN_USER_HOME}/CMakeModules/${extra_build_spec}
+	
+	conan_build_options="--settings build_type=${build_type}"
+	conan_profile_options="--profile=${profile_file}"
+	conan_install_options="--install-folder=${install_folder} ${conan_build_options} ${conan_build_cpp_options} --build outdated"
+	
+	echo "Installing all conan components using confile ${conanfile_txt}"
+	conan install ${conan_profile_options} ${conan_install_options} ${conanfile_txt}
+}
+
+######################################################################################
+# Installs a given set of Conan recipes
+#
+# Syntax: do_conan_install <conanfile> <Conan channel> <build type> <compiler> <compute engine> \
+#                          <optimization spec> <Conan profile> <Conan install folder>
+######################################################################################
+function do_conan_install_old()
+{
+	conanfile_txt=$1
+	conan_channel=$2
+	build_type=$3
+	compiler=$4
+	compute_engine=$5
+	optim_spec=$6
+	profile_file=$7
+	install_folder=$7
+	
+	if ! [[ "$optim_spec" =~ ^(all|safe|none)$ ]]
+	then
+		echo "Error: unknown optimisation level '$optim_spec'"
+		return 1
+	fi
+	
+	if ! [[ "$compute_engine" =~ ^(cpu|cuda_10)$ ]]
+	then
+		echo "Error: unknown compute engine type '$compute_engine'"
+		return 1
+	fi
+	
+	if [ "x$compiler" = "x" ]
+	then
+		echo "Error: no compiler specified"
+		return 1
+	fi
+
+	echo "Using profile '$profile_file' for building"
+	if [ ! -f "$profile_file" ]
+	then
+		echo "Error: profile file $profile_file does not exist"
+		return 1
+	fi
+	
+	if [ ! -f "$conanfile_txt" ]
+	then
+		echo "Error: conanfile $conanfile_txt does not exist"
+		return 1
+	fi
+
+	extra_build_spec=$(get_extra_build_spec $compiler $compute_engine $optim_spec)
+	
+	echo "Using extra build location '$extra_build_spec' for building"
+	install_folder=${CONAN_USER_HOME}/CMakeModules/${extra_build_spec}
+	conan_env_script=${install_folder}/activate_run.sh
+	if [ -f "$conan_env_script" ]
+	then
+		echo "Sourcing file ${conan_env_script}"
+		. ${conan_env_script}
+	fi
+	
+	##########################################################################
+	# Cleanup paths
+	##########################################################################
+	cleanupPath PATH
+	cleanupPath MANPATH
+	cleanupPath LIBRARY_PATH
+	cleanupPath DYLD_LIBRARY_PATH
+	cleanupPath LD_LIBRARY_PATH
+	cleanupPath CMAKE_PREFIX_PATH
+	
+	conan_build_options="--settings build_type=${build_type}"
+	conan_profile_options="--profile=${profile_file}"
+	conan_install_options="--install-folder=${install_folder} ${conan_build_options} ${conan_build_cpp_options} --build outdated"
+	
+	echo "Installing all conan components using confile ${conanfile_txt}"
+	conan install ${conan_profile_options} ${conan_install_options} ${conanfile_txt}
+}
